0e7fbf73a014bb151ea10acab7c0a6dc9481f8c4
George Mandis <george@mand.is>
2020-02-26 12:54:32 -0500

    Major refactor to StateContext

--- src/StateContext.js
+++ src/StateContext.js
@@ -2,11 +2,11 @@
 import React, { createContext, useEffect, useReducer, useState } from "react";
 
 // Import Reducers
-import gridReducer from "./reducer functions/gridReducer";
-import talkyBlockyReducer from "./reducer functions/talkyBlockyReducer";
+import gridReducer from "./reducers/gridReducer";
+import talkyBlockyReducer from "./reducers/talkyBlockyReducer";
 
 // Import helper functions
-import { colors } from "./helper functions/colors";
+import { directions, colors, nonCommands } from "./helpers/dictionary";
 
 // CREATE CONTEXT ////////////////////////////////////////////////////////////////////////////////////
 const StateContext = createContext();
@@ -14,133 +14,32 @@ const StateContext = createContext();
 function StateContextProvider(props) {
   // STATE //////////////////////////////////////////////////////////////////////////////////////////
   const [buildStage, setBuildStage] = useState(0);
-
   const [keywords, setKeyWords] = useState([]);
-  // const [keywordsIndex, setKeyWordsIndex] = useState(0);
-  let keywordsIndex = 0;
+  const [talkyIsTalking, setTalkyIsTalking] = useState(false);
+  const [talkyBackground, setTalkyBackground] = useState(false);
 
   const [grid, dispatchGrid] = useReducer(gridReducer, {});
-
   const [talkyBlocky, dispatchTalkyBlocky] = useReducer(talkyBlockyReducer, {});
-  const [talkyIsTalking, setTalkyIsTalking] = useState(false);
-  const [talkyBackground, setTalkyBackground] = useState(false);  
 
-
-  // SPEECH RECOGNITION API //////////////////////////////////////////////////////////////////////
-  let recognition;
-  let synth, talkyBlockyVoice;
-
-  const directions = {
-    up: "up",
-    down: "down",
-    left: "left",
-    right: "right",
-    rights: "right",
-    write: "right"
-  }
-
-  const nonCommands = {                        
-    // "walkie-talkie": "That's me!",
-    "hello":   ["Hi! How's it going?", "Yo!", "Hey there."],
-    // "high": ["Hi! How's it going?", "Yo!", "Hey there."],    
-    "george": "Hi George. It's so nice to hear your voice. I hope you are doing well... I love you.",
-    "yo": "Oh. Hi. Um. Is George still there?", // Dree
-    // jokes
-    "hear": "I like jokes. My life is a joke.", // want to hear a joke?
-    "binary": "Ha ha hahahaha haha hahahaha. Oh my god, that is so funny. hahahaha. I love you George.", // 10 types of people joke
-    "puns": "I don't know. Why don't thieves get puns?", // why don't thieves get puns?
-    "literally": "Oh. Ha.", // they take them them literally
-     "localhost": "Thanks for letting me present at localhost with you. I hope nothing goes wrong. That would be embarassing. For you.",
-     "thanks" : ["your welcome, easy fix", "no sweat, easy fix", "piece of cake", "It's not like I have a choice"],
-     "open": "I'm afraid I can't do that, George"
-  }
-
+  // SPEECH RECOGNITION //////////////////////////////////////////////////////////////////////
   const dictionary = [];
+  let keywordsIndex = 0;
+  let recognition, synth, talkyBlockyVoice;
 
   dictionary.push(...Object.keys(colors));
   dictionary.push(...Object.keys(directions));
-  dictionary.push(...Object.keys(nonCommands));  
+  dictionary.push(...Object.keys(nonCommands));
 
-  function talkyBlockySpeak(speech, probability=1) {
-    if (Math.random() < probability) {
-
-      // see if multiple responses are possible
-      if (typeof speech === 'object') {
-        speech = speech[Math.floor(Math.random() * speech.length)];
-      }
-
-      const utterThis = new SpeechSynthesisUtterance(speech);
-      utterThis.voice = talkyBlockyVoice;
-      utterThis.pitch = 1.5;
-      utterThis.rate = 0.75;    
-      synth.speak(utterThis);
-      setTalkyIsTalking(true);
-      utterThis.addEventListener("end", talkyStopsTalking);     
-          
-    }    
-  }
-
-
+  // SPEECH RECOGNITION //////////////////////////////////////////////////////////////////////
   function speechHandler(e) {
-    if (e.results[0].isFinal) {      
+    if (e.results[0].isFinal) {
       const words = e.results[0][0].transcript.toLowerCase().split(" ");
       console.log(words);
       const recognizedWords = words.filter(word => dictionary.includes(word));
 
       keywords.push(...recognizedWords);
-      setKeyWords(keywords);      
+      setKeyWords(keywords);
       processNewKeywords();
-    }
-  }
-
-  function talkyStopsTalking() {    
-    setTalkyIsTalking(false);
-  }
-
-  function talkyStartsListening() {    
-    setTalkyBackground("wtListening.png");
-  }
-
-  function talkyStopsListening() {
-    setTalkyBackground(false);    
-  }
-
-  // Some hacky shit to actually get the API to not stop when the speaker pauses.
-  function continuouslyTranscribe() {
-    recognition.start();
-  }
-
-  // KEY EVENTS //////////////////////////////////////////////////////////////////////////////
-
-  function spaceBarDownHandler(e) {
-    if (e.keyCode === 32) {
-      
-      try {                              
-        const sound = new Audio();
-        sound.src = "/assets/sounds/listenSound.wav";  
-        sound.volume = .015;
-        sound.play();
-    
-        recognition.start();
-        recognition.addEventListener("end", continuouslyTranscribe);              
-      }catch{
-        // recognition already started
-      }
-      
-    }
-  }
-
-  function spaceBarUpHandler(e) {
-
-    if (e.keyCode === 32) {      
-      const sound = new Audio();
-      sound.src = "/assets/sounds/executeSound.wav";  
-      sound.volume = .015;
-      sound.play();
-
-      recognition.removeEventListener("end", continuouslyTranscribe);
-      recognition.stop();
-      document.addEventListener("keydown", spaceBarDownHandler, { once: true });
     }
   }
 
@@ -150,91 +49,179 @@ function StateContextProvider(props) {
       const newKeywords = keywords.slice(keywordsIndex, keywords.length);
 
       if (newKeywords.length > 8) {
-        talkyBlockySpeak("You're lucky I'm a computer, because I would forget all of that otherwise.");
+        talkyBlockySpeak(
+          "You're lucky I'm a computer, because I would forget all of that otherwise."
+        );
       }
 
       newKeywords.forEach((keyword, index) => {
         console.log(keyword);
-        
-        setTimeout((keyword) => {
-          if (directions.hasOwnProperty(keyword)) {
-            dispatchTalkyBlocky({
-              type: "MOVE_TALKY_BLOCKY",
-              grid: grid,
-              direction: directions[keyword]
-            });
-            
-            setTalkyBackground(`wt${keyword}.png`);
-                          
-            if (newKeywords.length <= 8) {
-              talkyBlockySpeak(["Walk walk walk", ""], .25);
+
+        setTimeout(
+          keyword => {
+            if (directions.hasOwnProperty(keyword)) {
+              dispatchTalkyBlocky({
+                type: "MOVE_TALKY_BLOCKY",
+                grid: grid,
+                direction: directions[keyword]
+              });
+
+              setTalkyBackground(`wt${keyword}.png`);
+
+              if (newKeywords.length <= 8) {
+                talkyBlockySpeak(["Walk walk walk", ""], 0.25);
+              }
+            } else if (colors.hasOwnProperty(keyword)) {
+              dispatchGrid({
+                type: "CHANGE_GRID_BLOCK_COLOR",
+                position: [talkyBlocky.gridPos[0], talkyBlocky.gridPos[1]],
+                rgb: colors[keyword]
+              });
+
+              if (newKeywords.length <= 8) {
+                talkyBlockySpeak(
+                  [`Poof. It's ${keyword}`, `Voila.`, `${keyword} it is.`],
+                  0.25
+                );
+              }
+            } else if (nonCommands.hasOwnProperty(keyword)) {
+              talkyBlockySpeak(nonCommands[keyword]);
             }
 
-          } else if (colors.hasOwnProperty(keyword)) {
-            dispatchGrid({
-              type: "CHANGE_GRID_BLOCK_COLOR",
-              position: [talkyBlocky.gridPos[0], talkyBlocky.gridPos[1]],
-              rgb: colors[keyword]
-            });
-            
-            if (newKeywords.length <= 8) {
-              talkyBlockySpeak([`Poof. It's ${keyword}`, `Voila.`, `${keyword} it is.`], .25);
-            }
+            setBuildStage(Date.now());
+          },
+          index * 350,
+          keyword
+        );
 
-          } else if (nonCommands.hasOwnProperty(keyword)) {
-            talkyBlockySpeak(nonCommands[keyword]);
-          } 
-            
-          setBuildStage(Date.now());
-        }, index*350, keyword);
-
-        setTimeout(()=>{
+        setTimeout(() => {
           setTalkyBackground(`wtDefault.png`);
-        }, (newKeywords.length+1) * 350);
-        
+        }, (newKeywords.length + 1) * 350);
       });
 
       keywordsIndex = keywords.length;
-      // setKeyWordsIndex(keywordsIndex);
+    }
+  }
+
+  function talkyStartsListening() {
+    setTalkyBackground("wtListening.png");
+  }
+
+  function talkyStopsListening() {
+    setTalkyBackground(false);
+  }
+
+  // Some hacky shit to actually get the API to not stop when the speaker pauses.
+  function continuouslyTranscribe() {
+    recognition.start();
+  }
+
+  // SPEECH SYNTHESIS //////////////////////////////////////////////////////////////////////
+  function talkyBlockySpeak(speech, probability = 1) {
+    if (Math.random() < probability) {
+      // see if multiple responses are possible
+      if (typeof speech === "object") {
+        speech = speech[Math.floor(Math.random() * speech.length)];
+      }
+
+      const utterThis = new SpeechSynthesisUtterance(speech);
+      utterThis.voice = talkyBlockyVoice;
+      utterThis.pitch = 1.5;
+      utterThis.rate = 0.75;
+      synth.speak(utterThis);
+      setTalkyIsTalking(true);
+      utterThis.addEventListener("end", talkyStopsTalking);
+    }
+  }
+
+  function startTutorial(e) {
+    if (e.keyCode === 116) {
+      // 'T'
+      // talkyBlockyVoice.rate = 2
+      talkyBlockySpeak(
+        "Hi. I'm Talky Blocky! Hold down the spacebar to talk to me."
+      );
+      talkyBlockySpeak(
+        "If you say right, left, down or up I can move around the grid."
+      );
+      talkyBlockySpeak(
+        "Say the name of any color and I can change the grid blocks to that color!"
+      );
+      talkyBlockySpeak("You can also say hello and thanks.");
+      talkyBlockySpeak("Please speak clearly so that I can understand you.");
+      talkyBlockySpeak("Press 'T' to hear the tutorial again.");
+    }
+  }
+
+  function talkyStopsTalking() {
+    setTalkyIsTalking(false);
+  }
+
+  // KEY EVENTS //////////////////////////////////////////////////////////////////////////////
+  function spaceBarDownHandler(e) {
+    if (e.keyCode === 32) {
+      try {
+        const sound = new Audio();
+        sound.src = "/assets/sounds/listenSound.wav";
+        sound.volume = 0.015;
+        sound.play();
+
+        recognition.start();
+        recognition.addEventListener("end", continuouslyTranscribe);
+      } catch {
+        // recognition already started
+      }
+    }
+  }
+
+  function spaceBarUpHandler(e) {
+    if (e.keyCode === 32) {
+      const sound = new Audio();
+      sound.src = "/assets/sounds/executeSound.wav";
+      sound.volume = 0.015;
+      sound.play();
+
+      recognition.removeEventListener("end", continuouslyTranscribe);
+      recognition.stop();
+      document.addEventListener("keydown", spaceBarDownHandler, { once: true });
     }
   }
 
   // Initialize //////////////////////////////////////////////////////////////////////////////////
   useEffect(() => {
     if (buildStage === 0) {
-      dispatchGrid({ type: "BUILD_GRID" });      
+      dispatchGrid({ type: "BUILD_GRID" });
       setBuildStage(1);
     } else if (buildStage === 1) {
-      dispatchTalkyBlocky({ type: "BUILD_TALKY_BLOCKY", grid: grid });      
+      dispatchTalkyBlocky({ type: "BUILD_TALKY_BLOCKY", grid: grid });
       setBuildStage(2);
     } else if (buildStage === 2) {
       document.addEventListener("keydown", spaceBarDownHandler, { once: true });
       document.addEventListener("keyup", spaceBarUpHandler);
+      document.addEventListener("keypress", startTutorial);
 
-      
       // initializing the speech recognition API
       recognition = new window.webkitSpeechRecognition();
       recognition.continuous = false;
       recognition.lang = "en-US";
       recognition.interimResults = true;
       recognition.maxAlternatives = 1;
-      
+
       recognition.addEventListener("result", speechHandler);
       recognition.addEventListener("audiostart", talkyStartsListening);
       recognition.addEventListener("audioend", talkyStopsListening);
-            
+
       // initializing the speech synthesis API
-      synth = window.speechSynthesis; 
-            
-      synth.onvoiceschanged = () => {
-        const voices = synth.getVoices();      
-        voices.forEach((voice) => {        
-            if (voice.name === 'Google US English') {
-              talkyBlockyVoice = voice;
-              // talkyBlockySpeak("Hi! I'm Walkie-Talkie");
-            }
-          })          
-      };
+      synth = window.speechSynthesis;
+
+      synth.addEventListener("voiceschanged", () => {
+        const voices = synth.getVoices();
+        voices.forEach(voice => {
+          if (voice.name === "Google US English" && !talkyBlockyVoice) {
+            talkyBlockyVoice = voice;
+          }
+        });
+      });
 
       // adding grammar so talky blocky understands certain words more clearly than others
       const grammar = `#JSGF V1.0; grammar talkyblockyDictionary;  public <word> = ${dictionary.join(
@@ -246,15 +233,19 @@ function StateContextProvider(props) {
       recognition.grammars = speechRecognitionList;
 
       setBuildStage(3);
-    } else if (buildStage === 3) {      
+    } else if (buildStage === 3) {
+      alert(
+        "Hi. I'm Talky Blocky! Hold down the spacebar to talk to me. If you say right, left, down or up I can move around the grid. Say the name of any color and I can change the grid blocks to that color! You can also say hello and thanks. Please speak clearly so that I can understand you. Press 'T' to hear me speak this to you."
+      );
       setBuildStage(4);
     }
   }, [buildStage]);
 
-
   // PROVIDE CONTEXT //////////////////////////////////////////////////////////////////////////////
   return (
-    <StateContext.Provider value={{ grid, talkyBlocky, talkyIsTalking, talkyBackground}}>
+    <StateContext.Provider
+      value={{ grid, talkyBlocky, talkyIsTalking, talkyBackground }}
+    >
       {props.children}
     </StateContext.Provider>
   );
