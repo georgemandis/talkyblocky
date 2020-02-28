1b076ab3dec509e1c6b816d8c9d4beaff6be7bed
George Mandis <george@mand.is>
2020-02-28 17:53:03 -0500

    Published deployment branch

---
+++ precache-manifest.9349276893431990ea491fd6d7a26d1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bd46cdc8e5922a3a78cc0cf1824605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49acdd3db69b7d61353d",
+    "url": "/static/css/main.994b1dc7.chunk.css"
+  },
+  {
+    "revision": "7413868a17bae320445a",
+    "url": "/static/js/2.5313fe29.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5313fe29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49acdd3db69b7d61353d",
+    "url": "/static/js/main.569b8d13.chunk.js"
+  },
+  {
+    "revision": "dd56ee22934a25a84f14",
+    "url": "/static/js/runtime-main.ac296213.js"
+  }
+]);
